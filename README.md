9a1fc017e7924d592aa94cfccea762f4218d6e66
Fehmi Çitiloğlu <fehmicitiloglu@gmail.com>
2021-09-24 00:56:12 +0300

    readme added

---
+++ README.md
@@ -0,0 +1,16 @@
+### Github profile analyzer
+
+- To use just simply open index.html with your browser that support javascript
+
+* And then write username you wanna analyse to input area
+
+-> Here start page
+
+![image](./screenshots/giris.png)
+
+- if you write a username page looks like this..(I hope... At least it is looks like this in my computer)
+  ![image](./screenshots/result.png)
+
+* if you would write username that is not exist you gonna take an error like this..(but don't try 'asdasda' I don't know why it is not throwing any error)
+
+  ![image](./screenshots/result.png)
--- app.js
+++ app.js
@@ -1,34 +1,11 @@
+// birden fazla kullanılacak elementlerin global olarak seçilmesi
 const button = document.querySelector("header button");
 const languageList = document.querySelector("#languages");
 const repoInfo = document.querySelectorAll("#repo-info p  strong");
 const nameHTML = document.querySelector("#name").innerHTML;
 const userInfo = document.querySelector("#user-info");
 
-// const languageName = languageDiv.appendChild(document.createElement("p"));
-
-// const languagePercentage = languageDiv.appendChild(
-//   document.createElement("strong")
-// );
-
-// function createLanguageNode(language, percentage) {
-//   const languageList = document.querySelector("#languages");
-//   const languageDiv = languageList.appendChild(document.createElement("div"));
-//   const languageName = languageDiv.appendChild(document.createElement("p"));
-//   const languagePercentage = languageDiv.appendChild(
-//     document.createElement("strong")
-//   );
-//   const languageText = (languageName.text = language);
-//   const langPercentage = (languagePercentage.text = percentage) + "%";
-
-//   // console.log(languageList.children);
-// }
-// createLanguageNode("a", 2);
-// console.log(repoInfo[0].innerHTML);
-// console.log(repoInfo[1].innerHTML);
-
-// const uname = input.value;
-// FehmiCitiloglu
-
+// username, ad, soyad, bilgilerinin api'dan çekilmesi
 const getUserData = async (username) => {
   const url = "https://api.github.com/users/";
   const name = document.getElementById("name");
@@ -38,18 +15,17 @@ const getUserData = async (username) => {
   await fetch(url + username)
     .then((response) => response.json())
     .then((user) => {
-      // console.log(user);
       repoInfo[0].innerHTML = user.public_repos;
       profilePhoto.src = user.avatar_url;
       uname.innerHTML = "@" + user.login;
       name.innerHTML = user.name;
+    })
+    .catch((err) => {
+      throw new Error(err);
     });
-  // .catch((error) => {
-
-  //   errorHandler(error);
-  // });
 };
-
+// repolarda kullanılan dillerin api'dan çekilip ne kadar kullandığını
+// göstermek için yüzdelerin hesaplanması ve doma aktarma
 const getUserRepo = async (username) => {
   const repoRequest = "/repos?per_page=100";
   const url = "https://api.github.com/users/";
@@ -77,6 +53,7 @@ const getUserRepo = async (username) => {
         const numLanguage = userLanguages.filter(
           (language) => language === currLanguage
         );
+        console.log(numLanguage);
         if (currLanguage !== null) {
           const languageDiv = languageList.appendChild(
             document.createElement("div")
@@ -90,7 +67,6 @@ const getUserRepo = async (username) => {
           );
           const lanPercentage =
             ((numLanguage.length * 100) / totalItem).toFixed(2) + "%";
-          // createLanguageNode(currLanguage, langPercentage);
 
           languageName.innerHTML = currLanguage;
           languagePercentage.innerHTML = lanPercentage;
@@ -111,71 +87,41 @@ const getUserRepo = async (username) => {
     });
   repoInfo[1].innerHTML = (totalSize / 1000).toFixed(2) + " MB";
 };
-
-// let user = getUserData("FehmiCitiloglu", "avatar_url");
-// console.log(user.avatar_url);
-
+// girilen inputun submiti ve
 function handleInput(event) {
   const input = document.querySelector("header input");
 
   let inputValue = input.value;
 
+  console.log(inputValue);
+  // input kısmının boş olması durumunda fonksiyonu sonlandırmak için logic
+  if (inputValue.trim().length === 0) {
+    return;
+  }
   input.value = event.target.value;
 
-  // console.log(nameHTML);
   if (nameHTML !== undefined || nameHTML !== "") {
     removeInfos();
+
     userInfo.classList.remove("hidden-while-error");
   }
 
-  // try {
-  // const errorNode = document.querySelector("label");
-  // if (!errorNode) {
-  //   console.log("errorNode if block work");
-  //   errorNode.remove;
-  // }
-  // } catch (e) {
-  //   console.log("errorNode catch trigger");
-  // }
   return getUserData(inputValue), getUserRepo(inputValue);
 }
+// inputHandlerın buttona click event olarak verilmesi
 
 button.addEventListener("click", handleInput);
 
-// fetch(url + uname)
-//   .then((data) => data.json())
-//   .then((user) => {
-//     console.log("User loaded", user);
-//     profilePhoto.src = user.avatar_url;
-//     username.innerHTML = "@" + user.login;
-//     name.innerHTML = user.name;
-//   });
-
-// const denemeurl =
-//   "https://api.github.com/users/FehmiCitiloglu/repos?per_page=100";
-
-// fetch(denemeurl)
-//   .then((data) => data.json())
-//   .then((repos) => {
-//     console.log(repos);
-//     console.log(repos.map((repo) => repo.size));
-//     console.log(repos.map((repo) => repo.language));
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
+// ikinci bir arama yapılması durumunda domdaki önceki
+// aramaya ait elementlerin silinmesi
 function removeInfos() {
-  // console.log(languageList.children);
   const childList = [...languageList.children];
+
   childList.forEach((child) => child.remove());
-  // forEach((childNode) => {
-  //   childNode.remove();
-  // });
 }
+// kullanıcı bulunamadığı takdirde dönecek hata
 
 function errorHandler(errorMessage) {
-  // const mainContent = document.querySelector("main");
   const languageChildList = [...languageList.children];
 
   languageChildList.forEach((child) => child.remove());
@@ -183,8 +129,12 @@ function errorHandler(errorMessage) {
   const errorElement = document.createElement("label");
 
   languageList.appendChild(errorElement);
+
   errorElement.innerHTML = errorMessage;
+
   errorElement.classList = "error-handler";
+
   console.log(errorElement);
+
   userInfo.classList = "hidden-while-error";
 }
